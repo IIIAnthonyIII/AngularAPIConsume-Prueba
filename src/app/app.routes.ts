f28e11abffe040ced67b368b27172e9e2d7c5139
IIIAnthonyIII <aadvl382016@outlook.com>
2024-03-04 14:52:09 -0500

    LazyLoad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,11 @@ import { AutorFavoritoComponent } from './components/autor-favorito/autor-favori
 import { ObraFavoritaComponent } from './components/obra-favorita/obra-favorita.component';
 
 export const routes: Routes = [
-    { path: 'obras/:author', component: ObrasComponent },
-    { path: 'obras/:author/:title', component: DetailsObraComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'autorfavorito', component: AutorFavoritoComponent },
-    { path: 'obrafavorita', component: ObraFavoritaComponent },
-    { path: '', component: HomeComponent },
+    { path: 'obras/:author', loadComponent:() => import('./components/obras/obras.component').then(c => c.ObrasComponent) },
+    { path: 'obras/:author/:title', loadComponent:() => import('./components/details-obra/details-obra.component').then(c => c.DetailsObraComponent) },
+    { path: 'login', loadComponent:() => import('./components/login/login.component').then(c => c.LoginComponent) },
+    { path: 'autorfavorito', loadComponent:() => import('./components/autor-favorito/autor-favorito.component').then(c => c.AutorFavoritoComponent) },
+    { path: 'obrafavorita', loadComponent:() => import('./components/obra-favorita/obra-favorita.component').then(c => c.ObraFavoritaComponent) },
+    { path: '', loadComponent:() => import('./components/home/home.component').then(c => c.HomeComponent) },
     { path: '**', redirectTo: '' }
 ];
